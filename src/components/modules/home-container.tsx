082ef75f48a111fa8b4448f4d4363fbfe4b0383d
ba.nguyen <nguyenvanbapc66@gmail.com>
2022-05-01 20:41:27 +0700

    feat: ui home page

---
+++ src/components/modules/home-container.tsx
@@ -0,0 +1,80 @@
+import { Carousel } from 'antd';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+import BasicTemplateLayout from '@components/layouts/basic-template';
+import {
+  hiTeaLogo,
+  listHiTeaCoffee,
+  ListHiTeaCoffeeType,
+  listTeaCoffee,
+  slider,
+} from 'src/assets/images';
+import { MenuItemCard, MenuItemCategory } from '@components/core';
+import styles from './home-container.module.scss';
+
+type NewListHiTeaCoffee = {
+  itemsOfFirstRow: ListHiTeaCoffeeType[];
+  itemsOfSecondRow: ListHiTeaCoffeeType[];
+};
+
+const HomeContainer = () => {
+  const renderImageSlider = () => {
+    return Object.values(slider).map((urlImage) => <Image key={urlImage} src={urlImage} />);
+  };
+
+  const renderTeaCoffeeImages = () => {
+    return listTeaCoffee.map((data, index) => <MenuItemCategory key={index} data={data} />);
+  };
+
+  const formatDataHiTeaCoffee = () => {
+    const newListHiTeaCoffee: NewListHiTeaCoffee = {
+      itemsOfFirstRow: [],
+      itemsOfSecondRow: [],
+    };
+
+    listHiTeaCoffee.forEach((item, index) => {
+      if (index < 3) {
+        newListHiTeaCoffee.itemsOfFirstRow.push(item);
+      } else {
+        newListHiTeaCoffee.itemsOfSecondRow.push(item);
+      }
+    });
+    return newListHiTeaCoffee;
+  };
+
+  return (
+    <BasicTemplateLayout>
+      <Carousel autoplay swipeToSlide draggable>
+        {renderImageSlider()}
+      </Carousel>
+      <main className={clsx('my-20', styles.menuList)}>{renderTeaCoffeeImages()}</main>
+      <section className="bg-[#FFE9D4] py-16 mb-20">
+        <div className="flex flex-col items-center text-center">
+          <div className="max-w-[400px]">
+            <Image src={hiTeaLogo} />
+          </div>
+          <p className="max-w-[764px] font-semibold pt-3">
+            Được chiết xuất từ 100% hoa Hibiscus tự nhiên, không chứa caffeine cùng toppings trái
+            cây đa dạng, Hi-Tea Healthy là lựa chọn cho những ai muốn chăm sức khoẻ và yêu chiều bản
+            thân. Chọn Hi - Tea Healthy là chọn “Say Hi” với một cách sống mới “Heathy và Healing”
+            cho chính mình.
+          </p>
+          <p className="font-semibold text-[18px] pt-4">Thử đi chờ chi!</p>
+        </div>
+        <div className={styles.menuFirstCardList}>
+          {formatDataHiTeaCoffee().itemsOfFirstRow.map((data, index) => (
+            <MenuItemCard key={index} data={data} />
+          ))}
+        </div>
+        <div className={styles.menuSecondCardList}>
+          {formatDataHiTeaCoffee().itemsOfSecondRow.map((data, index) => (
+            <MenuItemCard key={index} data={data} className="max-w-[370px]" />
+          ))}
+        </div>
+      </section>
+    </BasicTemplateLayout>
+  );
+};
+
+export default HomeContainer;
